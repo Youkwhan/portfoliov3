ecf0187b4ea9386fe8cb4f3d8f46ed6675b35467
Youkwhan <Youkwhan@gmail.com>
2023-11-01 21:30:20 -0400

    style: Add google fonts and styles to root
    - Extended the tailwind color theme
    - used @apply custom styles from tailwind to root css file.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,24 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "bg-gray": "#E8E8E8",
+        "primary-100": "#676767",
+        "primary-200": "#333333",
+        "secondary-100": "#416BD8",
+        "primary-300": "#C4C4C4",
+      },
+      fontFamily: {
+        opensans: ["Open Sans", "sans-serif"],
+        lato: ["Lato", "sans-serif"],
+      },
+    },
+    screens: {
+      xs: "480px",
+      sm: "768px",
+      md: "1060px",
+    },
   },
   plugins: [],
 };
