ebf9b6e43eec0e7444509dd4925cb793dec27189
Youkwhan <Youkwhan@gmail.com>
2023-11-05 22:03:01 -0500

    feat: Added responsive mobile menu modal
    Added a mobile menu modal that displays a menu with links when the screen size is below the medium threshold and the menu button is toggled.
    Also added mobile menu items for navigation in the modal.

--- src/scenes/navbar/index.jsx
+++ src/scenes/navbar/index.jsx
@@ -39,14 +39,18 @@ function Navbar(selectedPage, setSelectedPage) {
               >
                 About
               </AnchorLink>
-              <a target="_blank" href="#">
+              <a
+                className="transition duration-500 hover:text-primary-300"
+                target="_blank"
+                href="#"
+              >
                 Resume
               </a>
             </div>
           ) : (
             // MENU BAR (mobile v.)
             <button
-              className="bg-secondary-100 rounded-full p-2"
+              className="rounded-full bg-primary-300 p-2"
               onClick={() => setIsMenuToggled(!isMenuToggled)}
             >
               <Bars3Icon className="h-6 w-6 text-white" />
@@ -54,6 +58,49 @@ function Navbar(selectedPage, setSelectedPage) {
           )}
         </div>
       </div>
+
+      {/* Mobile Menu Modal */}
+      {!isAboveMediumScreens && isMenuToggled && (
+        <div className="fixed bottom-0 right-0 z-40 h-full w-[300px] bg-primary-300 drop-shadow-xl">
+          {/* Close Icon */}
+          <div className="flex justify-end p-12">
+            <button onClick={() => setIsMenuToggled(!isMenuToggled)}>
+              <XMarkIcon className="h-6 w-6" />
+            </button>
+          </div>
+
+          {/* Menu Items */}
+          <div className="ml-[33%] flex flex-col gap-10 text-2xl">
+            <AnchorLink
+              className={`${
+                selectedPage === "home" ? "text-secondary-100" : ""
+              } transition duration-500 hover:text-primary-200`}
+              href="#home"
+              onClick={() => setSelectedPage("home")}
+            >
+              Home
+            </AnchorLink>
+            <AnchorLink
+              className={`${
+                selectedPage === "works" ? "text-secondary-100" : ""
+              } transition duration-500 hover:text-primary-200`}
+              href="#works"
+              onClick={() => setSelectedPage("works")}
+            >
+              Works
+            </AnchorLink>
+            <AnchorLink
+              className={`${
+                selectedPage === "about" ? "text-secondary-100" : ""
+              } transition duration-500 hover:text-primary-200`}
+              href="#"
+              onClick={() => setSelectedPage("about")}
+            >
+              About
+            </AnchorLink>
+          </div>
+        </div>
+      )}
     </nav>
   );
 }
