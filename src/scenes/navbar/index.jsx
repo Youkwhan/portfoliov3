2bc174cca4fe02273a04a048d315a1e6132461cb
Youkwhan <Youkwhan@gmail.com>
2023-11-05 21:34:43 -0500

    feat: Added responsive Navbar with useMediaQuery
    Implemented a responsive Navbar component with AnchorLinks for smooth scrolling.
    Utilized the useMediaQuery hook to handle responsiveness for different screen sizes, replacing CSS media queries.

--- src/scenes/navbar/index.jsx
+++ src/scenes/navbar/index.jsx
@@ -1,7 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import AnchorLink from "react-anchor-link-smooth-scroll";
+import useMediaQuery from "../../hooks/useMediaQuery";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid";
 
 function Navbar(selectedPage, setSelectedPage) {
+  const [isMenuToggled, setIsMenuToggled] = useState(false);
+  const isAboveMediumScreens = useMediaQuery("(min-width:1060px)");
+
   return (
     <nav>
       <div className="sticky top-0 z-30 w-full py-6 ">
@@ -14,29 +19,39 @@ function Navbar(selectedPage, setSelectedPage) {
             youkwhan kim
           </AnchorLink>
 
-          <div className="flex items-center justify-end gap-8 text-sm">
-            <AnchorLink
-              className={`${
-                selectedPage === "works" ? "text-secondary-100" : ""
-              } transition duration-500 hover:text-primary-300`}
-              href="#works"
-              onClick={() => setSelectedPage("works")}
+          {isAboveMediumScreens ? (
+            <div className="flex items-center justify-end gap-8 text-sm">
+              <AnchorLink
+                className={`${
+                  selectedPage === "works" ? "text-secondary-100" : ""
+                } transition duration-500 hover:text-primary-300`}
+                href="#works"
+                onClick={() => setSelectedPage("works")}
+              >
+                Works
+              </AnchorLink>
+              <AnchorLink
+                className={`${
+                  selectedPage === "about" ? "text-secondary-100" : ""
+                } transition duration-500 hover:text-primary-300`}
+                href="#"
+                onClick={() => setSelectedPage("about")}
+              >
+                About
+              </AnchorLink>
+              <a target="_blank" href="#">
+                Resume
+              </a>
+            </div>
+          ) : (
+            // MENU BAR (mobile v.)
+            <button
+              className="bg-secondary-100 rounded-full p-2"
+              onClick={() => setIsMenuToggled(!isMenuToggled)}
             >
-              Works
-            </AnchorLink>
-            <AnchorLink
-              className={`${
-                selectedPage === "about" ? "text-secondary-100" : ""
-              } transition duration-500 hover:text-primary-300`}
-              href="#"
-              onClick={() => setSelectedPage("about")}
-            >
-              About
-            </AnchorLink>
-            <a target="_blank" href="#">
-              Resume
-            </a>
-          </div>
+              <Bars3Icon className="h-6 w-6 text-white" />
+            </button>
+          )}
         </div>
       </div>
     </nav>
