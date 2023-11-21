e7bd8af25b3cf8f99a178211e235e4882834e86a
Youkwhan <Youkwhan@gmail.com>
2023-11-20 21:38:43 -0500

    feat(ErrorPage): Error component

--- src/scenes/errorPage/index.jsx
+++ src/scenes/errorPage/index.jsx
@@ -5,7 +5,10 @@ export default function ErrorPage() {
   console.error(error);
 
   return (
-    <div id="error-page">
+    <div
+      id="error-page"
+      className="flex flex-col items-center justify-center text-center"
+    >
       <h1>Oops!</h1>
       <p>Sorry, an unexpected error has occurred.</p>
       <p>
